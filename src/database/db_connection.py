40d52216b0ba3f9a069b7f86014126639b0ff406
Le Van An <levanannt79@gmail.com>
2025-10-11 18:34:17 +0700

    commit

--- src/database/db_connection.py
+++ src/database/db_connection.py
@@ -1,3 +1,42 @@
 import psycopg2
+import json
+import os
 
-print("Database connection module loaded.")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+json_path = os.path.join(current_dir, "config.json")
+
+with open(json_path, "r", encoding="utf-8") as f:
+    config = json.load(f)
+
+DATABASE = config["DB_CONNECTION"]
+host = DATABASE["HOST"]
+port = DATABASE["PORT"]
+database = DATABASE["DATABASE"]
+user = DATABASE["USER"]
+password = DATABASE["PASSWORD"]
+
+
+def connect_db():
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            host=host,
+            database=database,
+            port=port,
+            user=user,
+            password=password,
+        )
+        print("Database connection successful.")
+    except psycopg2.Error as e:
+        print(f"Error connecting to the database: {e}")
+    return conn
+
+
+def close_db(conn):
+    if conn:
+        conn.close()
+        print("Database connection closed.")
+
+
+conn = connect_db()
+close_db(conn)
